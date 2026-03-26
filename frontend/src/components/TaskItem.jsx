3cb21f172536ca6e3bed61ab2b648b0bc6f813b5
DDale21 <christiandalepancho@gmail.com>
2026-03-26 18:35:14 +1300

    feat: add updating of task title.

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -1,15 +1,70 @@
-import { Trash2, MoveLeft, MoveRight } from "lucide-react";
+import { Pencil, Trash2, MoveLeft, MoveRight, Pen } from "lucide-react";
+import { useState } from "react";
 
 const TaskItem = ({ _id, title, status, onDelete, onUpdate }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [titleValue, setTitleValue] = useState(title);
+
+  const handleTitleChange = (e) => {
+    setTitleValue(e.target.value);
+  };
+
+  const handleSaveClick = () => {
+    onUpdate(_id, titleValue);
+    setIsEditing(false);
+  };
+
+  const handleCancelClick = () => {
+    setIsEditing(false);
+  };
+
   return (
     <>
       <li key={_id}>
         <div className="task-container">
           <div className="task-content">
-            <div>{title}</div>
-            <button className="delete-button" onClick={() => onDelete(_id)}>
-              <Trash2 size={18} />
-            </button>
+            {isEditing ? (
+              <>
+                <input
+                  className="task-edit-input"
+                  type="text"
+                  value={titleValue}
+                  onChange={handleTitleChange}
+                />
+                <div className="task-controls">
+                  <button
+                    className="task-edit-save-button"
+                    onClick={handleSaveClick}
+                  >
+                    Save
+                  </button>
+                  <button
+                    className="task-edit-cancel-button"
+                    onClick={handleCancelClick}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </>
+            ) : (
+              <>
+                <span>{titleValue}</span>
+                <div className="task-controls">
+                  <button
+                    className="edit-button"
+                    onClick={() => setIsEditing(true)}
+                  >
+                    <Pencil size={18} />
+                  </button>
+                  <button
+                    className="delete-button"
+                    onClick={() => onDelete(_id)}
+                  >
+                    <Trash2 size={18} />
+                  </button>
+                </div>
+              </>
+            )}
           </div>
 
           <div className="task-status-update-container">
